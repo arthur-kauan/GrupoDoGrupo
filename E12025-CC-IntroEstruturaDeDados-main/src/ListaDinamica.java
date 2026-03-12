f5e0f3bdacade59677194d629ddadd40b3d28dc3
Natan <natan.domareski@edu.unifil.br>
2026-03-12 20:47:57 -0300

    Alterado o -ultimoIndiceDe -contarOcorrencias -substituir -

--- E12025-CC-IntroEstruturaDeDados-main/src/ListaDinamica.java
+++ E12025-CC-IntroEstruturaDeDados-main/src/ListaDinamica.java
@@ -1,4 +1,4 @@
-public class ListaDinamica {
+public class ListaDinamica implements ListaOperacoes {
     No inicio;
 
     public ListaDinamica() {
@@ -72,14 +72,125 @@ public class ListaDinamica {
         No aux = this.inicio;
 
         do {
-            if(aux.getConteudo().equals(elemento)) {
+            if (aux.getConteudo().equals(elemento)) {
                 System.out.println("Elemento " + elemento + " encontrado.");
                 return true;
             }
             aux = aux.getProx();
-        } while(aux != null);
+        } while (aux != null);
         System.out.println("Elemento " + elemento + " não encontrado!");
         return false;
     }
 
+    @Override
+    public int removerTodas(String elemento) {
+    int i = 0;
+    if (!existeInicio()) return 0;
+
+    // 1. Trata remoções no início da lista (o "novo" início pode ser o elemento também)
+    while (this.inicio != null && this.inicio.getConteudo().equals(elemento)) {
+        this.inicio = this.inicio.getProx();
+        i++;
+    }
+
+    // 2. Trata remoções no restante da lista
+    if (this.inicio != null) {
+        No ant = this.inicio;
+        No aux = this.inicio.getProx();
+
+        while (aux != null) {
+            if (aux.getConteudo().equals(elemento)) {
+                ant.setProx(aux.getProx());
+                i++;
+                aux = ant.getProx();
+            } else {
+                ant = aux;
+                aux = aux.getProx();
+            }
+        }
+    }
+    return i;
+    }
+
+    @Override
+    public int contar() {
+        return 0;
+    }
+
+    @Override
+    public int adicionarVarios(String[] elementos) {
+        return 0;
+    }
+
+    @Override
+    public String obter(int indice) {
+        return "";
+    }
+
+    @Override
+    public boolean inserir(int indice, String elemento) {
+        return false;
+    }
+
+    @Override
+    public String removerPorIndice(int indice) {
+
+
+        return "";
+    }
+
+    @Override
+    public void limpar() {
+
+
+    }
+
+    @Override
+    public int ultimoIndiceDe(String elemento) {
+        No aux = this.inicio;
+        int i = 0;
+        int ultimoIndice = 0;
+        while (aux != null && aux.getConteudo() != null)
+        {
+            if(aux.getConteudo().equals(elemento))
+            {
+                ultimoIndice = i;
+            }
+            aux = aux.getProx();
+            i++;
+        }
+        return ultimoIndice;
+    }
+
+    @Override
+    public int contarOcorrencias(String elemento) {
+        int i = 0;
+        if (existeInicio()) {
+            No aux = this.inicio;
+
+            while (aux != null && aux.getConteudo() != null) {
+                if (aux.getConteudo().equals(elemento)) {
+
+                    i++;
+                }
+                aux = aux.getProx();
+            }
+        }
+        return i;
+    }
+
+    @Override
+    public int substituir(String antigo, String novo) {
+        No aux = this.inicio;
+        int i = 0;
+        do{
+            if(aux.getConteudo().equals(antigo))
+            {
+                aux.setConteudo(novo);
+                i++;
+            }
+            aux = aux.getProx();
+        }while (aux != null && aux.getConteudo() != null);
+        return i;
+    }
 }
--- E12025-CC-IntroEstruturaDeDados-main/src/MainListaDinamica.java
+++ E12025-CC-IntroEstruturaDeDados-main/src/MainListaDinamica.java
@@ -2,8 +2,15 @@ public class MainListaDinamica {
     public static void main(String[] args) {
         ListaDinamica listaDinamica = new ListaDinamica();
         listaDinamica.adicionarElemento("Arroz");
+        listaDinamica.adicionarElemento("Arroz");
+        listaDinamica.adicionarElemento("Arroz");
+        listaDinamica.adicionarElemento("Arroz");
+        listaDinamica.adicionarElemento("Arroz");
+        listaDinamica.adicionarElemento("Arroz");
+        listaDinamica.adicionarElemento("Tucunare");
         listaDinamica.adicionarElemento("Feijão");
         listaDinamica.removerElemento("Feijão");
+        System.out.println(listaDinamica.removerTodas("Arroz"));
         listaDinamica.exibirElementos();
     }
 }
